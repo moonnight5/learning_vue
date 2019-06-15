2fc084d52e7479fc50bb1f941310c12b64f0403f
moonnight5 <1696442886@qq.com>
2019-06-15 20:46:01 +0800

    add

---
+++ todomvc/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <counter />
+  </div>
+</template>
+<script>
+// 两个兄弟组件，共享count数据
+
+import Counter from './components/Counter';
+// import CounterBrother from './components/CounterBrother';
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  components: {
+    'counter': Counter,
+    // 'counter-brother': CounterBrother
+  },
+  methods: {
+    // incrementCounter () {
+    //   this.count++;
+    // }
+  },
+}
+</script>
+<style>
+  
+</style>
