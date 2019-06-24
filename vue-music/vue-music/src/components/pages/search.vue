52ce4e4e70cabb438454bdf909a54f891c37371e
moonnight5 <1696442886@qq.com>
2019-06-24 14:37:54 +0800

    modified

--- vue-music/vue-music/src/components/pages/search.vue
+++ vue-music/vue-music/src/components/pages/search.vue
@@ -4,19 +4,46 @@
       <!-- 搜索框 -->
       <searchBox></searchBox>
     </div>
+    <div class="shortcut-wrapper" ref="shortcutWrapper" v-show="!query" >
+      <!-- 热门搜索 -->
+      <scroll class="shortcut" ref="shortcut" :data="shortcut" :refreshDelay="refreshDelay">
+        <div class="hot-key">
+          <h1 class="title">热门搜索</h1>
+          <ul>
+            <li
+              class="item"
+              v-for="(item, index) in hotKey" :key="index"
+              @click="addQuery(item.first)"
+            >
+              <span>{{item.first}}</span>
+            </li>
+          </ul>
+        </div>
+      </scroll>
+    </div>
   </div>
 </template>
 
 <script>
 import searchBox from '@/components/searchBox'
+import scroll from '@/components/scroll'
 export default {
   data() {
     return {
-      
+      query: false,
+      hotKey: [
+        {first: '0新歌发布'},
+        {first: '1新歌发布'},
+        {first: 'a新歌发布'},
+        {first: 'b新歌发布'},
+        {first: 'c新歌发布'},
+        {first: 'd新歌发布'},
+      ]
     }
   },
   components: {
-    'searchBox': searchBox
+    'searchBox': searchBox,
+    'scroll': scroll
   }
 }
 </script>
