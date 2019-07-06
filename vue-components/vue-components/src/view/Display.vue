1c5c585c7e889df4e7a73fdcb4d3d636bf0a082c
moonnight5 <1696442886@qq.com>
2019-07-06 10:05:17 +0800

    add

---
+++ vue-components/vue-components/src/view/Display.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>动态渲染 .vue 文件的组件 --display</h3>
+    <Display :code="code"></Display>
+  </div>
+</template>
+
+<script>
+import display from '@/components/com/display'
+import defaultCode from './default-code'
+export default {
+  components: {
+    'Display': display
+  },data() {
+    return {
+      code: defaultCode
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
